62bf721442c87665e7d9e56539324cf7dcadf44f
Jude Beadle <info@jbeadle.de>
2018-07-20 19:56:18 +0200

    adding sw.js with code from https://www.youtube.com/watch?v=jsGs9z7TuyY

---
+++ sw.js
@@ -0,0 +1,31 @@
+var staticCacheName = 'restaurant-cache-1';
+
+let urlToCache = [
+	'/',
+	'/restaurant.html',
+	'/css/styles.css',
+	'/data/restaurants.json',
+	'/img/1.jpg',
+	'/img/2.jpg',
+	'/img/3.jpg',
+	'/img/4.jpg',
+	'/img/5.jpg',
+	'/img/6.jpg',
+	'/img/7.jpg',
+	'/img/8.jpg',
+	'/img/9.jpg',
+	'/img/10.jpg',
+	'/js/main.js',
+	'/js/restaurant_info.js',
+	'/js/dbhelper.js',
+];
+self.addEventListener('install', function(event){
+	event.waitUntil(
+		caches.open(staticCacheName).then(function(cache){
+			console.log(cache);
+			return cache.addAll(urlToCache);
+		}).catch(error => {
+			console.log(error)
+		})
+	);
+});
