7cbe4a2abd0bfed78e8a21a17c6bfdba41c5ee6d
Jude Beadle <info@jbeadle.de>
2018-07-20 20:04:28 +0200

    adding map token key to detail view

--- sw.js
+++ sw.js
@@ -19,6 +19,7 @@ let urlToCache = [
 	'/js/restaurant_info.js',
 	'/js/dbhelper.js',
 ];
+
 self.addEventListener('install', function(event){
 	event.waitUntil(
 		caches.open(staticCacheName).then(function(cache){
@@ -29,3 +30,11 @@ self.addEventListener('install', function(event){
 		})
 	);
 });
+
+self.addEventListener('fetch', function(event){
+	event.respondWith(
+		caches.match(event.request).then(function(response){
+			return response || fetch(event.request);
+		})
+	);
+})
